11421642075939aa29d396e67e531fcf24d50181
Elad Keyshawn <elad.keyshawn@gmail.com>
2016-05-20 05:41:20 +0300

    Improvments

--- PlateReader/app/src/main/java/com/projects/elad/platereader/CroppedPreviewActivity.java
+++ PlateReader/app/src/main/java/com/projects/elad/platereader/CroppedPreviewActivity.java
@@ -3,25 +3,28 @@ package com.projects.elad.platereader;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import java.io.IOException;
 
 public class CroppedPreviewActivity extends AppCompatActivity implements View.OnClickListener {
-
+  private  ProgressBar progressBar;
   private ImageView croppedImage;
   private Button resultsButton;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_cropped_preview);
-
+    progressBar = (ProgressBar) findViewById(R.id.progress_bar);
     resultsButton = (Button) findViewById(R.id.show_results_button);
     Uri resultUri = Uri.parse(getIntent().getExtras().getString("resultUri"));
     croppedImage = (ImageView) findViewById(R.id.cropped_preview);
@@ -41,10 +44,28 @@ public class CroppedPreviewActivity extends AppCompatActivity implements View.On
   @Override
   public void onClick(View v) {
 
-    Intent intent = new Intent(this,ResultsActivity.class );
-    startActivity(intent);
 
+    Handler handler = new Handler();
+
+// Create and start a new Thread
+    progressBar.setVisibility(View.VISIBLE);
+    handler = new Handler();
+
+
+
+    handler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        progressBar.setVisibility(View.GONE);
+        Intent intent = new Intent(getBaseContext(),ResultsActivity.class );
+        startActivity(intent);
+      }
+    }, 4000);
   }
+
+
+
+
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
